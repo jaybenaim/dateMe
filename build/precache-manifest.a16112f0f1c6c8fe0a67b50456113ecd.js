4a2ed0a58d6ede6401088c6e83d7fa6ca40f3deb
jaybenaim <benaimjacob@gmail.com>
2020-07-23 14:08:23 -0400

    changed homepage type

---
+++ build/precache-manifest.a16112f0f1c6c8fe0a67b50456113ecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b30adb596f4f63a6a5e0258ed802f0a",
+    "url": "/react-starter-template/index.html"
+  },
+  {
+    "revision": "b87287c9d8ba22f225fa",
+    "url": "/react-starter-template/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "f6e7e7cab4b147900e44",
+    "url": "/react-starter-template/static/css/main.4904b276.chunk.css"
+  },
+  {
+    "revision": "b87287c9d8ba22f225fa",
+    "url": "/react-starter-template/static/js/2.d2629bea.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/react-starter-template/static/js/2.d2629bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e7e7cab4b147900e44",
+    "url": "/react-starter-template/static/js/main.687a3c88.chunk.js"
+  },
+  {
+    "revision": "495a17ee549bc779519d",
+    "url": "/react-starter-template/static/js/runtime-main.39979ba4.js"
+  }
+]);
